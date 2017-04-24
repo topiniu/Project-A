cfc82c57298a7d553ee31318ffbc82f59066a4c9
topiniu <topiniu@outlook.com>
2017-04-24 19:14:30 +0800

    数据传递解决好了

--- js/dataUtil.js
+++ js/dataUtil.js
@@ -7,19 +7,20 @@ function uploadData(){
     var addQurl = "http://localhost:8080/Project_B/ajax/addQuestion";
     var addAurl = "http://localhost:8080/Project_B/ajax/addAnswers";
     // var addRAurl = "http://localhost:8080/Project_B/ajax/addQuestion";
-    xhr.open("POST",addQurl,true);
     xhr.onreadystatechange = function(){
         if(xhr.readyState===4){
             console.log(xhr.responseText);
         }
     }
-    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
 
     for(var q in QUESTIONDATA){
         var item = QUESTIONDATA[q];
         var data = "id=" + item._id+
             "&content="+item._content+
             "&managerId="+item._managerId;
+
+        xhr.open("POST",addQurl,false);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
         xhr.send(data);
 
         data ="answers="+item._answers+
@@ -27,7 +28,8 @@ function uploadData(){
             "&questionId="+item._id;
 
 
-        xhr.open("POST",addAurl,true);
+        xhr.open("POST",addAurl,false);
+        xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
         xhr.send(data);
     }
 }
