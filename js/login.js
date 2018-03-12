039ac1efa4247106d0cd13bafa0dd836875814ff
topiniu <topiniu@outlook.com>
2018-03-12 10:57:53 +0800

    优化登陆页

--- js/login.js
+++ js/login.js
@@ -2,6 +2,16 @@
  * Created by topiniu on 2017/4/19.
  */
 function login(){
-    console.log('hello')
-    window.location.href = 'html/manager.html'
+    
+    var id = $('.j_idbox').val();
+    var pass = $('.j_passbox').val();
+
+    if(id === 'topiniu' && pass == 'topiniu123')
+        window.location.href = 'html/manager.html'
+    else{
+        $('.j_msgbox').html('wrong id or password');
+        $('.j_msgbox').css('opacity',1);
+        setTimeout(function(){$('.j_msgbox').css('opacity',0)},2000);
+        console.log('hh')
+    }
 }
