7ea114e87b988d41547b9fec308472ba4b19ebe2
topiniu <topiniu@outlook.com>
2018-03-12 10:24:34 +0800

    不知道更新了啥

--- js/login.js
+++ js/login.js
@@ -2,18 +2,6 @@
  * Created by topiniu on 2017/4/19.
  */
 function login(){
-    var idBox = document.getElementById("idBox");
-    var passBox = document.getElementById("passBox");
-
-    console.log(idBox.value);
-    if(idBox.value==="" || passBox.value===""){
-        var messsBox = document.getElementsByClassName("messageBox")[0];
-        messsBox.style.animationName = "showHide";
-        setTimeout(function () {
-            messsBox.style.animationName = "";
-        },3100);
-    }else{
-        localStorage.setItem("loginedUser",idBox.value);
-        window.location.href = "Manager.html?backurl="+window.location.href;
-    }
+    console.log('hello')
+    window.location.href = 'html/manager.html'
 }
